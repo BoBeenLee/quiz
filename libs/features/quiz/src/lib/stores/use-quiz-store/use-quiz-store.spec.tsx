d0b73b8648f9488c73cc074863f4725c368df8c4
bobeenlee <globaldev@naver.com>
2022-09-07 11:32:55 +0900

    feat: 퀴즈 스토어 테스트케이즈 작성

--- libs/features/quiz/src/lib/stores/use-quiz-store/use-quiz-store.spec.tsx
+++ libs/features/quiz/src/lib/stores/use-quiz-store/use-quiz-store.spec.tsx
@@ -1,6 +1,5 @@
-import { act } from 'react-dom/test-utils';
 import { MOCK_QUIZ_ITEMS } from '../../__mocks__/quiz';
-import { useQuizStore } from './use-quiz-store';
+import { quizSummary, useQuizStore } from './use-quiz-store';
 
 describe('useQuizStore', () => {
   beforeEach(() => {
@@ -8,54 +7,67 @@ describe('useQuizStore', () => {
     getState().reset();
   });
 
-  test('should empty quizStore', () => {
+  test('퀴즈 문제가 비어있을 경우', () => {
     const { getState } = useQuizStore;
     expect(getState().quizItems.length).toBe(0);
     expect(getState().quizItemsAt).toBe(null);
   });
 
-  test('should not empty quizStore', () => {
+  test('퀴즈 문제가 존재할 경우', () => {
     const { getState } = useQuizStore;
-
-    act(() => {
-      getState().setQuizItems(MOCK_QUIZ_ITEMS);
-    });
+    getState().setQuizItems(MOCK_QUIZ_ITEMS);
 
     expect(getState().quizItems.length).toBe(MOCK_QUIZ_ITEMS.length);
     expect(getState().quizItemsAt).not.toBe(null);
   });
 
-  test('should be setAnswer', () => {
+  test('첫번째 퀴즈 문제 답안을 작성했을 경우', () => {
     const { getState } = useQuizStore;
+    getState().setQuizItems(MOCK_QUIZ_ITEMS);
+    getState().setAnswer(
+      MOCK_QUIZ_ITEMS[0].id,
+      MOCK_QUIZ_ITEMS[0].incorrectAnswers[0]
+    );
 
-    act(() => {
-      getState().setQuizItems(MOCK_QUIZ_ITEMS);
-      getState().setAnswer(MOCK_QUIZ_ITEMS[0].id, 'test');
-    });
-
-    expect(getState().quizItems[0].answer).toBe('test');
+    expect(getState().quizItems[0].answer).toBe(
+      MOCK_QUIZ_ITEMS[0].incorrectAnswers[0]
+    );
   });
 
-  test('should be retry', () => {
+  test('모든 퀴즈 문제 작성후 퀴즈 통계 데이터 확인하는 경우', () => {
     const { getState } = useQuizStore;
+    getState().setQuizItems(MOCK_QUIZ_ITEMS);
+    for (const item of MOCK_QUIZ_ITEMS) {
+      getState().setAnswer(item.id, item.incorrectAnswers[0]);
+    }
+    const { totalQuizCount, correctCount, incorrectCount } = quizSummary(
+      getState()
+    );
+    expect(totalQuizCount).toBe(10);
+    expect(correctCount).toBe(0);
+    expect(incorrectCount).toBe(10);
+  });
 
-    act(() => {
-      getState().setQuizItems(MOCK_QUIZ_ITEMS);
-      getState().setAnswer(MOCK_QUIZ_ITEMS[0].id, 'test');
-      getState().retry();
-    });
+  test('퀴즈 문제를 다시풀기할 경우', () => {
+    const { getState } = useQuizStore;
+    getState().setQuizItems(MOCK_QUIZ_ITEMS);
+    getState().setAnswer(
+      MOCK_QUIZ_ITEMS[0].id,
+      MOCK_QUIZ_ITEMS[0].incorrectAnswers[0]
+    );
+    getState().retry();
 
     expect(getState().quizItems[0].answer).toBe(undefined);
   });
 
-  test('should be reset', () => {
+  test('퀴즈 문제들 리셋할 경우', () => {
     const { getState } = useQuizStore;
-
-    act(() => {
-      getState().setQuizItems(MOCK_QUIZ_ITEMS);
-      getState().setAnswer(MOCK_QUIZ_ITEMS[0].id, 'test');
-      getState().reset();
-    });
+    getState().setQuizItems(MOCK_QUIZ_ITEMS);
+    getState().setAnswer(
+      MOCK_QUIZ_ITEMS[0].id,
+      MOCK_QUIZ_ITEMS[0].incorrectAnswers[0]
+    );
+    getState().reset();
 
     expect(getState().quizItems.length).toBe(0);
     expect(getState().quizItemsAt).toBe(null);
