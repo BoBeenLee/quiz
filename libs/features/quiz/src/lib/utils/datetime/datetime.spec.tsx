d7f3d0a758fe91cbc4e71c3ecba2e27065646774
bobeenlee <globaldev@naver.com>
2022-09-07 11:35:21 +0900

    feat: datetime 테스트케이스 추가

--- libs/features/quiz/src/lib/utils/datetime/datetime.spec.tsx
+++ libs/features/quiz/src/lib/utils/datetime/datetime.spec.tsx
@@ -1,13 +1,27 @@
 import { getMinutesAndSeconds, todayTime } from './datetime';
 
 describe('datetime', () => {
-  it('should be getMinutesAndSeconds 10minutes 0seconds', () => {
+  test('should be getMinutesAndSeconds 10minutes 0seconds', () => {
     const { minutes, seconds } = getMinutesAndSeconds(10 * 60 * 1000);
     expect(minutes).toBe(10);
     expect(seconds).toBe(0);
   });
 
-  it('should be todayTime', () => {
+  test('should be getMinutesAndSeconds 0minutes 10seconds', () => {
+    const { minutes, seconds } = getMinutesAndSeconds(10 * 1000);
+    expect(minutes).toBe(0);
+    expect(seconds).toBe(10);
+  });
+
+  test('should be getMinutesAndSeconds 0minutes 10seconds', () => {
+    const { minutes, seconds } = getMinutesAndSeconds(
+      10 * 60 * 1000 + 10 * 1000
+    );
+    expect(minutes).toBe(10);
+    expect(seconds).toBe(10);
+  });
+
+  test('should be todayTime', () => {
     expect(todayTime()).toBe(todayTime());
   });
 });
